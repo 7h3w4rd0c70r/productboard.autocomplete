34a2fcae3e9d650b9acf3a483894c3f139444ef3
Patrik Šimunič <patriksimunic@hotmail.com>
2018-01-28 15:35:22 +0100

    Add text to layout

--- src/dumb/layout.jsx
+++ src/dumb/layout.jsx
@@ -6,6 +6,7 @@ import {
     Form,
     Row,
 } from 'antd'
+import cx from 'classnames'
 
 import InputSmart from '../smart/input'
 
@@ -13,8 +14,20 @@ const FormItem = Form.Item
 
 export default function LayoutDumb() {
     return (
-        <div>
-            <br/><br/><br/><br/>
+        <div className={cx('layout')}>
+            <Row gutter={20}>
+                <Col span={10} offset={7}>
+                    <p className={cx('description', 'header')}>
+                        Autocomplete Example
+                    </p>
+                    <p className={cx('description')}>
+                        This autocomplete example uses <strong>Google API</strong> to guess what you are trying to type in.
+                    </p>
+                    <p className={cx('description')}>
+                        The input it self is in two variations - raw html input with dataoptions without any special styling and second variation is using select styled with AntD component <a href='https://ant.design/components/auto-complete/' target='_blank'>AutoComplete</a> for a nice user interface.
+                    </p>
+                </Col>
+            </Row>
             <Row gutter={20}>
                 <Col span={8} offset={8}>
                     <FormItem>
